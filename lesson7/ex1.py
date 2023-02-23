499b907517b3aea2345b04f4826c124f927abdfa
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2023-02-23 21:35:09 +0300

    Chapter Seven

---
+++ lesson7/ex1.py
@@ -0,0 +1,27 @@
+class Room:
+
+    class WinDoor:
+        def __init__(self, w, h):
+            self.square = w * h
+
+    def __init__(self, x, y, z):
+        self.square = 2 * z * (x + y)
+        self.wd = []
+
+    def add_wd(self, w, h):
+        self.wd.append(Room.WinDoor(w, h))
+
+    def work_surface(self):
+        new_square = self.square
+        for i in self.wd:
+            new_square -= i.square
+        return new_square
+
+r1 = Room(6, 3, 2.7)
+print(r1.square)
+r1.add_wd(1, 1)
+r1.add_wd(1, 1)
+r1.add_wd(1, 2)
+
+print(r1.work_surface())
+
