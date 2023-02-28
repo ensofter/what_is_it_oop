16901936bd88618854cc90c6295147d276257e65
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2023-02-28 22:37:14 +0300

    Chapter Twelve

--- lesson12/cop_client.py
+++ lesson12/cop_client.py
@@ -2,7 +2,7 @@ from circuit_of_responsibility import AbstractLoadHandler
 
 
 class HardwareLoadHandler(AbstractLoadHandler):
-    def handle(self, request: dict) -> bools:
+    def handle(self, request: dict) -> bool:
         stat = request.get('stat')
         if stat is None:
             stat = {}
@@ -15,13 +15,31 @@ class HardwareLoadHandler(AbstractLoadHandler):
             return False
 
 class BiosLoadHandler(AbstractLoadHandler):
-    def handle(self, request: dict) -> bools:
+    def handle(self, request: dict) -> bool:
         stat = request.get('stat')
-        if stati is None:
+        if stat is None:
             stat = {}
         bios = request.get('bios')
         stat['bios'] = bios != ''
+        request['stat'] = stat
         if bios != '':
             super().handle(request)
         else:
             return False
+
+
+if __name__ == '__main__':
+    hw_handler = HardwareLoadHandler()
+    bios_handler = BiosLoadHandler()
+
+
+    hw_handler.set_next(bios_handler)
+
+    request = {
+        'cpu': 'Intel',
+        'bios': ''
+    }
+
+    hw_handler.handle(request)
+
+    print(request)
