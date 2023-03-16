f6bb9a69402d2ab20673673ae259271e6f928e34
Romashov Aleksey <bogestvenniyiri@yandex.ru>
2023-03-16 14:37:40 +0300

    wow

---
+++ solid/ex5.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class Relationship(Enum):
+    PARENT = 0
+    CHILD = 1
+    SIBLING = 2
+
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+
+class RelationBrowser(ABC):
+    @abstractmethod
+    def find_all_children_of(self, name: str):
+        pass
+
+
+class RelationStorage(RelationBrowser):
+    def __init__(self):
+        self.relations = []
+
+    def add_parent_and_child(self, parent: Person, child: Person):
+        self.relations.append((parent, Relationship.PARENT, child))
+        self.relations.append((child, Relationship.CHILD, parent))
+
+    def find_all_children_of(self, name: str):
+        for i in self.relations:
+            if i[0].name == name and i[1] == Relationship.PARENT:
+                yield i[2].name
+
+
+class Research:
+    def __init__(self, relations: RelationStorage):
+        self.relations = relations.relations
+        for i in self.relations:
+            if i[0].name == "John" and i[1] == Relationship.PARENT:
+                print(f"John is parent of {i[2].name}")
+
+
+if __name__ == '__main__':
+    parent = Person('John')
+    child1 = Person('Fill')
+    child2 = Person('Sofi')
+
+    relationships = RelationStorage()
+    relationships.add_parent_and_child(parent, child1)
+    relationships.add_parent_and_child(parent, child2)
+
+    research = Research(relationships)
